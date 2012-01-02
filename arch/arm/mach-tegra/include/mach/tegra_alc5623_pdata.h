81fb681c0a49834825dad48984e8a81f64906311
Jason Stern <jasonkstern@gmail.com>
2012-01-01 22:48:31 -0500

    Revert "Revert "Creating branch for sound testing. ALC5623 is seen, but does not play sound.""
    This reverts commit d09ce3a0af289a870ced8bf21b0f2b01339b1681.

---
+++ arch/arm/mach-tegra/include/mach/tegra_alc5623_pdata.h
@@ -0,0 +1,25 @@
+/*
+ * arch/arm/mach-tegra/include/mach/tegra_alc5623_pdata.h
+ *
+ * Author: Jason Stern
+ *
+ * Based on code copyright/by:
+ *
+ * Copyright 2011 NVIDIA, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+struct tegra_alc5623_platform_data {
+	int gpio_spkr_en;
+	int gpio_hp_det;
+	int gpio_int_mic_en;
+};
