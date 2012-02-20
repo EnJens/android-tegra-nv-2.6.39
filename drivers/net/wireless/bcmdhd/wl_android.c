db14d23ad628584238186495170fbfd9058b8c1b
David O'Neil <drmon89@gmail.com>
2012-02-20 22:37:45 +1100

    Reduce wifi delay

--- drivers/net/wireless/bcmdhd/wl_android.c
+++ drivers/net/wireless/bcmdhd/wl_android.c
@@ -373,7 +373,6 @@ onretry:
 ////////////////////////////////// Adam Patch /////////////////////////////////////
 	if (ret == -EIO && retry < 5) {
 		wl_android_wifi_off(dev);
-		msleep(1000);
 		printk("I/O Error on wifi, power down and try again. Attempt %d/5\n", retry);
 		goto onretry;
 	}
